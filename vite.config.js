56bb839e56fe2b5feb338d89670b89cc50698a88
ravipal <palravi1093@gmail.com>
2025-03-22 17:37:09 +0530

    Fix: Import defineConfig in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0', // Bind to all interfaces
-    port: process.env.PORT || 4173, // Use Render's PORT or default to 4173
-    strictPort: true,
-  },
-});
+  plugins: [react()],
+})
