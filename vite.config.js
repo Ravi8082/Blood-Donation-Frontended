d6369bfbb18ffd43bd153fbcc88baa3c840f2b88
ravipal <palravi1093@gmail.com>
2025-03-22 17:49:10 +0530

    Fix allowedHosts for Render

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,  // Allows external access
+    strictPort: true,
+    allowedHosts: "blood-donation-frontended-7.onrender.com"
+  }
 })
