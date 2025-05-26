a12963d4f0f1b253c724749e056413f89668eed8
Ravi Pal <palravi1093@gmail.com>
2025-05-26 19:51:16 +0530

    Update AdminDashboard.jsx

--- src/Component/AdminDashboard/AdminDashboard.jsx
+++ src/Component/AdminDashboard/AdminDashboard.jsx
@@ -46,10 +46,10 @@ const AdminDashboard = () => {
 
       // Fetch all required data
       const [donorRes, requestRes, userRes, bloodRes] = await Promise.all([
-        axios.get("https://blood-donation-backend-19.onrender.com/donorlist"),
-        axios.get("https://blood-donation-backend-19.onrender.com/requestHistory"),
-        axios.get("https://blood-donation-backend-19.onrender.com/userlist"),
-        axios.get("https://blood-donation-backend-19.onrender.com/bloodDetails"),
+        axios.get("http://localhost:8082"/donorlist"),
+        axios.get("http://localhost:8082"/requestHistory"),
+        axios.get("http://localhost:8082"/userlist"),
+        axios.get("http://localhost:8082"/bloodDetails"),
       ]);
 
       // Set state for lists
