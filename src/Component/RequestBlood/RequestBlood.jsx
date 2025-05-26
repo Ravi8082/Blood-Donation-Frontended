c3c3acd5e8df1fecce17a9f93853c51b0f8a1bcf
Ravi Pal <palravi1093@gmail.com>
2025-05-26 20:19:43 +0530

    Update RequestBlood.jsx

--- src/Component/RequestBlood/RequestBlood.jsx
+++ src/Component/RequestBlood/RequestBlood.jsx
@@ -104,7 +104,7 @@ const RequestBlood = () => {
         if (!validateForm()) return;
 
         try {
-            await axios.post('http://localhost:8082"/requestblood', request);
+            await axios.post('http://localhost:8082/requestblood', request);
             setSuccess(true);
             setFailed(false);
             setMsg('Blood Request Sent Successfully!');
